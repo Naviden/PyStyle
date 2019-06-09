942c2adedfdc4d8d8ad7f4c0849fc1e11c68f6bd
navid.nobani@gmail.com <navid.nobani@gmail.com>
2019-06-09 19:52:28 +0200

    add .

--- main.py
+++ main.py
@@ -1,55 +1,35 @@
 
+def color_guide():
+    print('\n')
+    print('\u001b[1mStandard Colors\u001b[0m\n')
+    for j in range(0, 8):
+        code = str(j)
+        print(f"\u001b[48;5;{code}m {code.center(8)}", end='')
+    print("\u001b[0m")
 
-print('\n')
-print('\u001b[1mStandard Colors\u001b[0m\n')
-for j in range(0, 8):
-    code = str(j)
-    # print(code)
-    print(f"\u001b[48;5;{code}m {code.center(8)}", end='')
-print("\u001b[0m")
+    print('\n')
+    print('\u001b[1mHigh-Intensity Colors\u001b[0m\n')
+    for j in range(8, 16):
+        code = str(j)
+        print(f"\u001b[48;5;{code}m {code.center(8)}", end='')
+    print("\u001b[0m")
 
-print('\n')
-print('\u001b[1mHigh-Intensity Colors\u001b[0m\n')
-for j in range(8, 16):
-    code = str(j)
-    # print(code)
-    print(f"\u001b[48;5;{code}m {code.center(8)}", end='')
-print("\u001b[0m")
+    print('\n')
+    print('\u001b[1mColors\u001b[0m\n')
+    for m in range(0, 6):
+        for n in range(0, 36):
+            code = str(m * 36 + (n + 16))
+            print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
+        print("\u001b[0m")
 
-print('\n')
-print('\u001b[1mColors\u001b[0m\n')
-for j in range(16, 52):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
-print("\u001b[0m")
-for j in range(52, 88):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
-print("\u001b[0m")
-for j in range(88, 124):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
-print("\u001b[0m")
-for j in range(124, 160):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
-print("\u001b[0m")
-for j in range(160, 196):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
-print("\u001b[0m")
-for j in range(196, 232):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(3)}", end='')
-print("\u001b[0m")
+    print('\n')
+    print('\u001b[1mGrayscale colors\u001b[0m\n')
+    for j in range(232, 256):
+        code = str(j)
+        print(f"\u001b[48;5;{code}m {code.ljust(5)}", end='')
+    print("\u001b[0m")
 
-print('\n')
-print('\u001b[1mGrayscale colors\u001b[0m\n')
-for j in range(232, 256):
-    code = str(j)
-    print(f"\u001b[48;5;{code}m {code.ljust(5)}", end='')
-print("\u001b[0m")
-
+color_guide()
 
 def text_formatter(text, bc=82, tc=0, bold=False, underline=False, reversed=False):
     if bold:
@@ -66,7 +46,3 @@ def text_formatter(text, bc=82, tc=0, bold=False, underline=False, reversed=Fals
         r = ''
 
     return(f'{b}{u}{r}\u001b[48;5;{bc}m\u001b[38;5;{tc}m{text}\u001b[0m')
-
-
-# ass = text_formatter('shunbul')
-# print(f'asghar memadi {ass} e maas!')
