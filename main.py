7c7f5f2c395d8156f81c565cbe3cd5a92330150d
navid.nobani@gmail.com <navid.nobani@gmail.com>
2019-06-09 13:36:52 +0200

    remove old lines from main

--- main.py
+++ main.py
@@ -1,37 +1,5 @@
 
-import sys
 
-
-def change_color(fun):
-    def color(text):
-        return fun(f"\033[1;32;10m{text}\033[0;37;10m ")
-    return color
-
-
-@change_color
-def print_color(text):
-    # print(text)
-    return text
-
-
-a = print_color('asghar memadi')
-# print(f'nkjnk knkjnk kjnkjnkj {a} kjnkjnk kjbjkjk kjnk ')
-
-# text color
-# for i in range(0, 16):
-#     for j in range(0, 16):
-#         code = str(i * 16 + j)
-#         # print(code)
-#         print(f"\u001b[38;5;{code}m {code.ljust(4)}", end='')
-#     print ("\u001b[0m")
-
-# background color
-# for i in range(0, 16):
-#     for j in range(0, 16):
-#         code = str(i * 16 + j)
-#         # print(code)
-#         print(f"\u001b[48;5;{code}m {code.ljust(5)}", end='')
-#     print("\u001b[0m")
 print('\n')
 print('\u001b[1mStandard Colors\u001b[0m\n')
 for j in range(0, 8):
